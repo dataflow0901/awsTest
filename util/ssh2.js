54f5925dc2ef02c2ab31ddc6992f5925870af62a
dataflow0901 <dataflow0901>
2020-11-03 17:11:38 +0900

    add bash profile command before blockchain command

--- util/ssh2.js
+++ util/ssh2.js
@@ -79,6 +79,7 @@ module.exports.queryAccount = (data) => {
     * userId: string
     */
    const command = `
+        source ~/.bash_profile;
         docker exec -e "CORE_PEER_TLS_ENABLED=true" \
         -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem"  \
         -e "CORE_PEER_LOCALMSPID=$MSP" \
